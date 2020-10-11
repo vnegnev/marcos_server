e7de6b3785fd1bf4d0bfd6a34d0e6815cf0cfd2d
Vlad Negnevitsky <vnegnev at geemail>
2020-10-11 16:38:18 +0200

    Device displayed based on clock freq

--- src/marcos_server.cpp
+++ src/marcos_server.cpp
@@ -22,6 +22,10 @@ iface *ifa;
 
 int main(int argc, char *argv[]) {
 	std::cout << "MaRCoS server, " << __DATE__ << " " << __TIME__ << std::endl;
+	std::cout << "FPGA clock: " << FPGA_CLK_FREQ_HZ/1e6 << " MHz" << std::endl;
+	if (FPGA_CLK_FREQ_HZ == 125e6) std::cout << "Red Pitaya 125-14" << std::endl;
+	else if (FPGA_CLK_FREQ_HZ == 122.88e6) std::cout << "StemLAB 122.88-16" << std::endl;
+	else std::cout << "UNKNOWN DEVICE" << std::endl;
 
 	// Global version string creation
 	std::stringstream sv;
@@ -29,7 +33,7 @@ int main(int argc, char *argv[]) {
 	SERVER_VERSION_UINT = ((VERSION_MAJOR << 16) & 0xff0000) | ((VERSION_MINOR << 8) & 0xff00) | (VERSION_DEBUG & 0xff);
 	SERVER_VERSION_STR = sv.str();
 	
-	std::cout << "Version " << SERVER_VERSION_STR << std::endl;
+	std::cout << "Server version " << SERVER_VERSION_STR << std::endl;
 
 	hw = new hardware();
 	ifa = new iface();
